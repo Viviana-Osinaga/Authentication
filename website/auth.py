7af6399e340e084036b1c0474e8350152efb86e6
viviana <vivianasolizosinaga@gmail.com>
2024-10-05 15:34:48 -0300

    checking if user is logged in

--- website/auth.py
+++ website/auth.py
@@ -22,7 +22,8 @@ def login():
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-    return render_template("login.html", boolean=True)
+
+    return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
 @login_required     #we cannot access 'auth.login' (logout) if we're not logged in.
@@ -57,4 +58,4 @@ def sign_up():
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
